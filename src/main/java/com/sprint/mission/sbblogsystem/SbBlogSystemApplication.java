9776c583e996d9594e112cbbc4020ceeafc18b8b
KimSeeun <9035anny@naver.com>
2025-03-24 15:48:28 +0900

    chore: 프로젝트 기본 환경 설정

---
+++ src/main/java/com/sprint/mission/sbblogsystem/SbBlogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.sbblogsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBlogSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBlogSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/mission/sbblogsystem/SbBlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.sbblogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
